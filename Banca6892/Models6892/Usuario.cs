a38d97203bd772e65c67ac0feb6dc3a809b30800
Luis Leyva <lmorales6@ucol.mx>
2022-04-27 12:51:27 -0500

    Avance

--- Banca6892/Models6892/Usuario.cs
+++ Banca6892/Models6892/Usuario.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace Banca6892.Models6892
 {
     internal class Usuario
     {
+        public string Nombre { get; set; }
+
+        public string ApellidoPaterno { get; set; }
+
+        public string ApellidoMaterno { get; set; }
+
+        public ObservableCollection<Telefono> Telefonos { get; set; }
+    }
+
+    public class Telefono
+    {
+        public string Id { get; set; }
+
+        public string Numero { get; set; }
+
     }
 }
